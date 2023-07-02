71146be24300a3681089c6f6044b1234de998720
Ruturaj <ruturajhb2003@gmail.com>
2023-07-02 11:38:34 +0530

    Add Functionality To Buttons

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+const quoteContainer = document.getElementById(
+    'quote-container'
+);
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
-const newQuoteBtn = document.getElementById('quote');
+
 // Show new Quotes
 
 function newQuote() {
     // Pick a random quote from ApiQuotes array
     const quote =
         apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    // Check for Null Val of Author
+    if (!quote.author) {
+        authorText.innerText = 'Unkown';
+    } else {
+        authorText.innerText = quote.author;
+    }
+
+    // Chwck Quote Length to determine styling {
+    if (quote.text.length > 150) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = quote.text;
 }
+
 // Get Quotes From Api
 async function getQuotes() {
     const apiUrl =
@@ -19,5 +42,14 @@ async function getQuotes() {
     } catch (error) { }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 // ON load
 getQuotes();
